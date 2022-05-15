d29809953c4f07588687afaf5d1f7e762cd6d28e
krishpy3 <98233937+krishpy3@users.noreply.github.com>
2022-05-15 15:36:28 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-	    res.send('Hello Multi Universe from a Node.js app!')
+	host_name = process.env.MYSQL_HOST
+	    res.send(`Hello Multi Universe - ${host_name} from a Node.js app!`)
 })
 
 app.listen(3000,'0.0.0.0', () => {
