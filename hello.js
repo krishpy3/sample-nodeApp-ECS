c8182828bd44b1f048f7ca6394ae4bb4eedcda12
Krish <krishpy3@gmail.com>
2022-06-18 14:49:09 +0530

    db update

--- hello.js
+++ hello.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-	host_name = process.env.MYSQL_HOST
-	    res.send(`Hello Multi Universe - ${host_name} from a Node.js app!`)
+	res.send(`Hello <br>
+	Host: ${process.env.DATABASE_HOST},<br>
+	DB: ${process.env.DATABASE_USER},<br> 
+	Name: ${process.env.DATABASE_NAME},<br>
+	Pwd: ${process.env.DATABASE_PASSWORD},<br>
+	from a Node.js app!`)
 })
 
-app.listen(3000,'0.0.0.0', () => {
-	    console.log('Server is up on 3000')
+app.listen(3000, '0.0.0.0', () => {
+	console.log('Server is up on 3000')
 })
